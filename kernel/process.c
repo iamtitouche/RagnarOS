148d208a2338cbb940e6cc388d25760edbb64776
titouan <titouan.sadoun@grenoble-inp.org>
2024-07-07 23:06:14 +0200

    Partie utilisation de listes de processus

--- kernel/process.c
+++ kernel/process.c
@@ -6,7 +6,8 @@
 
 struct processus* proc_elu; 
 struct processus* table_processus[TAILLE_TABLE_PROCESSUS];
-struct processus* queue_table;
+struct processus* head_activable;
+struct processus* queue_activable;
 
 struct processus processus0;
 struct processus processus1;
@@ -19,32 +20,56 @@ int mon_pid() {
     return proc_elu->pid;
 }
 
-int32_t cree_processus(void (*code)(void), char *nom) {
-    if (!queue_table) {
-        table_processus[0] = malloc(sizeof(struct processus));
-        queue_table = table_processus[0];
-        queue_table->pid = 0;
-        strcpy(queue_table->nom, nom);
-        queue_table->etat = ELU;
-        proc_elu = queue_table;
+int32_t add_proc_activable(struct processus *proc) {
+    if (proc->etat != ACTIVABLE) {
+        return -1;
+    }
+    if (queue_activable) {
+        queue_activable->next = proc;
     } else {
-        int new_pid = queue_table->pid + 1;
+        head_activable = proc;
+    }
+    queue_activable = proc;
+    return queue_activable->pid;
+}
+
+void rm_head_activable() {
+    struct processus *tmp = head_activable;
+    head_activable = head_activable->next;
+    tmp->next = NULL;
+}
+
+int32_t cree_processus(void (*code)(void), char *nom) {
+    if (!proc_elu) {
+        table_processus[0] = malloc(sizeof(struct processus));
+        proc_elu = table_processus[0];
+        proc_elu->pid = 0;
+        strcpy(proc_elu->nom, nom);
+        proc_elu->etat = ELU;
+        return proc_elu->pid;
+    } else if (proc_elu && !head_activable) {
+        table_processus[1] = malloc(sizeof(struct processus));
+        head_activable = table_processus[1];
+        queue_activable = table_processus[1];
+        queue_activable->pid = 1;
+    } else {
+        int new_pid = queue_activable->pid + 1;
 
         if (new_pid >= TAILLE_TABLE_PROCESSUS) {
             return -1;
         }
 
         table_processus[new_pid] = malloc(sizeof(struct processus));
-        queue_table->next = table_processus[new_pid];
-        queue_table = table_processus[new_pid];
-        queue_table->pid = new_pid;
-        strcpy(queue_table->nom, nom);
-        queue_table->etat = ACTIVABLE;
-
-        queue_table->pile[TAILLE_PILE_EXEC - 1] = (int) code;
-        queue_table->contexte[1] = (int) &(queue_table->pile[TAILLE_PILE_EXEC - 1]);
+        queue_activable->next = table_processus[new_pid];
+        queue_activable = table_processus[new_pid];
+        queue_activable->pid = new_pid;
     }
-    return queue_table->pid;
+    strcpy(queue_activable->nom, nom);
+    queue_activable->etat = ACTIVABLE;
+
+    queue_activable->pile[TAILLE_PILE_EXEC - 1] = (int) code;
+    queue_activable->contexte[1] = (int) &(queue_activable->pile[TAILLE_PILE_EXEC - 1]);
+    return queue_activable->pid;
 }
 
 
@@ -72,8 +97,9 @@ void proc2(void) {
 
 
 void init_processus(){
-    queue_table = NULL;
-    cree_processus(idle, "idle");
+    queue_activable = NULL;
+    head_activable = NULL;
+    cree_processus(idle, "idle large name");
     cree_processus(proc1, "proc1");
     cree_processus(proc2, "proc2");
 
@@ -87,18 +113,22 @@ void init_processus(){
 void ordonnance(void) {
     struct processus *next_proc;
 
-    if (proc_elu->next) {
-        next_proc = proc_elu->next;
-    } else {
-        next_proc = table_processus[0];
-    }
+    if (head_activable) {
+        //défensif
+        if (head_activable->etat != ACTIVABLE) {
+            printf("Tête des activables non activable");
+            return;
+        }
+        next_proc = head_activable;
+        proc_elu->etat = ACTIVABLE;
+        next_proc->etat = ELU;
+        add_proc_activable(proc_elu);
+        rm_head_activable();
 
-    proc_elu->etat = ACTIVABLE;
-    next_proc->etat = ELU;
+        struct processus* tmp = proc_elu; 
+        proc_elu = next_proc;
 
-    struct processus* tmp = proc_elu; 
-    proc_elu = next_proc;
-
-    ctx_sw(&(tmp->contexte[0]), &(next_proc->contexte[0]));
+        ctx_sw(&(tmp->contexte[0]), &(next_proc->contexte[0]));
+    } 
 }
 
